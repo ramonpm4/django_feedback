6e0eb816816aa742d99482a5fd77780102d5fd61
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-11 20:03:20 -0300

    added reviews to INSTALLED_APPS

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
-def review():
-    pass
+def review(request) -> HttpResponse:
+    return render(request, 'reviews/review.html')
