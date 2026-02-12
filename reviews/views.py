dc07160dd7e2549da0ecdcd3305dd15ce193d01d
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-12 14:45:35 -0300

    if request.method == 'POST' then extract data and redirect

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(request) -> HttpResponse:
+    if request.method == 'POST':
+        entered_username = request.POST['username'] # recorda que viene como dictionary con key: username porque yo le puse ese nombre.
+        print(entered_username)
+        return HttpResponseRedirect('/thank-you')
+        
     return render(request, 'reviews/review.html')
 
 
