5dde3cededc1a8320156c74e4c09133e289ecf8d
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-12 15:12:55 -0300

    null username validation: has_error as a parameter in review.html

--- reviews/views.py
+++ reviews/views.py
@@ -7,10 +7,18 @@ from django.http import HttpResponseRedirect
 def review(request) -> HttpResponse:
     if request.method == 'POST':
         entered_username = request.POST['username'] # recorda que viene como dictionary con key: username porque yo le puse ese nombre.
+        
+        if entered_username == '':
+            return render(request, 'reviews/review.html', {
+                'has_error': True
+            })
+        
         print(entered_username)
         return HttpResponseRedirect('thank-you')
         
-    return render(request, 'reviews/review.html')
+    return render(request, 'reviews/review.html', {
+        'has_error': False
+    })
 
 
 def thank_you(request) -> HttpResponse:
