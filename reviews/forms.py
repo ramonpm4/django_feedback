9c739a553983094c01d463438ed3e020b341aa46
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-13 11:54:49 -0300

    More controls in forms + review.html for tag

--- reviews/forms.py
+++ reviews/forms.py
@@ -4,5 +4,6 @@ from django import forms
 class ReviewForm(forms.Form):
     user_name = forms.CharField(label='Your Name', max_length=20, error_messages= {
         'required': 'This field is required motherfucker!',
-        'max_length': 'Your name is too long.'
-    })
+        'max_length': 'Your name is too long.'})
+    review_text = forms.CharField(label='Your Feedback', widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label='Your Rating', min_value=1, max_value=5)
