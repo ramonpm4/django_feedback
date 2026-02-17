420a8465ef7e67fb8e0af472a278ad1e430c3417
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-17 15:10:26 -0300

    Form

--- profiles/views.py
+++ profiles/views.py
@@ -3,12 +3,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
+from .forms import ProfileForm
+
 # Create your views here.
 
 
 class CreateProfileView(View):
     def get(self, request) -> HttpResponse:
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            'form': form
+        })
 
     def post(self, request) -> None:
         print(request.FILES['image'])
