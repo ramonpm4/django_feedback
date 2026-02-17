737302d77b9038d9a793f443846b130539de9f3d
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-17 15:28:30 -0300

    FileField

--- feedback/settings.py
+++ feedback/settings.py
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'uploads' # Construye el absolute path al uploads folder.
--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.views import View
 
 from .forms import ProfileForm
+from .models import UserProfile
+
 
 # Create your views here.
 
@@ -16,5 +18,13 @@ class CreateProfileView(View):
         })
 
     def post(self, request) -> None:
-        print(request.FILES['image'])
-        return HttpResponseRedirect('/profiles') # type: ignore
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES['user_image'])
+            profile.save()
+            return HttpResponseRedirect('/profiles') # type: ignore
+        
+        return render(request, "profiles/create_profile.html", {
+            'form': submitted_form
+        }) # type: ignore
